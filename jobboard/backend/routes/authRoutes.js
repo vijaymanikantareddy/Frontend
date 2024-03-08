d4b630db97e0c39aa73e103b1ceabe37e88fcfc1
vijaymanikantareddy <svmreddy7799@gmail.com>
2024-03-08 14:48:39 +0530

    SignIn, SignUP, LogOut

--- jobboard/backend/routes/authRoutes.js
+++ jobboard/backend/routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { signin } = require("../controllers/authController");
 const router = express.Router();
+const { signup, signin, logout } = require("../controllers/authController");
 
-//Auth Routes
-router.get("/", signin);
+//auth routes
+// /api/signup
+router.post("/signup", signup);
+// /api/signin
+router.post("/signin", signin);
+// /api/logout
+router.get("/logout", logout);
 
 module.exports = router;
